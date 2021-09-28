af6921b422634f728d63ff57aca1b6aea5f2b151
programmer-heeney <programmer.heeney@gmail.com>
2021-09-28 10:55:38 +0900

    2015 최신 모듈 (export, import)

---
+++ 5-module/app.js
@@ -0,0 +1,9 @@
+// import { increase, getCount } from './counter.js';
+import * as counter from './counter.js';
+// console.log(count);
+// console.log(getCount());
+
+counter.increase();
+counter.increase();
+counter.increase();
+console.log(counter.getCount());
